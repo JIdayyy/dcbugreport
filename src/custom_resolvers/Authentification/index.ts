792f38d6e63453179536a7b9aaa7c8e00693ac46
JIdayyy <julien_abbadie@hotmail.fr>
2022-04-06 19:51:35 +0200

    :bug: (import)

--- src/custom_resolvers/Authentification/index.ts
+++
@@ -1,4 +0,0 @@
-export { LoginResolver } from './LoginResolver';
-// export { LogoutResolver } from './logout';
-// export { MeResolver } from './me';
-// export { RegisterResolver } from './register';
