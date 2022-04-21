7cbf834d60511a0ed9dafb7e6e570149cbba815b
JIdayyy <julien_abbadie@hotmail.fr>
2022-04-21 11:40:15 +0200

    :bug: (eslint)

--- src/custom_resolvers/Args/createBugFromWidgetArgs.ts
+++ src/custom_resolvers/Args/createBugFromWidgetArgs.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import * as TypeGraphQL from 'type-graphql';
 import { CreateBugFromWidgetInputs } from '../Inputs/createBugFromWidgetInputs';
 
