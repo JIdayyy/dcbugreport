c4554e8bde5ef324ab1ddae558a31ea23d8be949
JIdayyy <julien_abbadie@hotmail.fr>
2022-04-24 08:56:39 +0200

    fix: 🐛 domain cookie fix

--- src/services/authentication.ts
+++ src/services/authentication.ts
@@ -141,10 +141,10 @@ export const setCookieToken = (token: string, ctx: GQLContext): void => {
     httpOnly: process.env.NODE_ENV === 'production',
     secure: process.env.NODE_ENV === 'production',
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-    // domain:
-    //   process.env.NODE_ENV === 'production'
-    //     ? 'dcbugreport.vercel.app'
-    //     : 'localhost',
+    domain:
+      process.env.NODE_ENV === 'production'
+        ? 'bug-rep.digitalcopilote.re'
+        : 'localhost',
   });
 
   ctx.res.setHeader('Access-Control-Allow-Credentials', 'true');
